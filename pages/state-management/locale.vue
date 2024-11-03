e45ebc93ce99fa5eeb1106ad6fbfaf0744b2aa92
Romi Julianto <romyjulians@gmail.com>
2024-11-04 05:57:48 +0700

    feat: add locale management with dynamic locale selection and date formatting

---
+++ pages/state-management/locale.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const locales = useLocales()
+const locale = useLocale()
+const date = useLocaleDate(new Date('2016-10-26'))
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt birthday</h1>
+    <p>{{ date }}</p>
+    <label for="locale-chooser">Preview a different locale</label>
+    <select id="locale-chooser" v-model="locale">
+      <option v-for="locale of locales" :key="locale" :value="locale">
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+</template>
