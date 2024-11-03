ea00866a4142f095aa6649eb9c91092ad4782193
Romi Julianto <romyjulians@gmail.com>
2024-11-03 15:35:17 +0700

    feat: add about page, navigation component, and dynamic post/user routes

---
+++ pages/nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/">Post 1</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/nav">Post 2</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
+<template>
+    <h1>Post {{ route.params.id }}</h1>
+    <p>This is the content of post {{ route.params.id }}</p>
+</template>
