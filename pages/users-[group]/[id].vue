e2266a98f70fba0348312af6f9a67117c1c1328a
Romi Julianto <romyjulians@gmail.com>
2024-11-03 15:39:15 +0700

    feat: add authentication warning for admin users in user detail page

--- pages/users-[group]/[id].vue
+++ pages/users-[group]/[id].vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+
+if (route.params.group === 'admins') {
+    console.log('Warning! Make sure user is authenticated!')
+}
+</script>
+
 <template>
     <p>{{ $route.params.group }} - {{ $route.params.id }}</p>
 </template>
