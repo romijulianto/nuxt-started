7e44eaf57bdc8e8ea764a2eabd828170698a17b8
Romi Julianto <romyjulians@gmail.com>
2024-11-03 16:27:07 +0700

    feat: add Open Graph meta tag for dynamic page titles in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+
+useHead({
+    meta: [{ property: 'og:title', content: `App Name - ${route.meta.title}` }]
+})
+</script>
+
 <template>
     <div>
         <div>Header</div>
