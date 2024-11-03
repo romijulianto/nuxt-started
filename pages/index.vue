e0435bc49ac93dbe36e07bd4455bafb46bb0a51e
Romi Julianto <romyjulians@gmail.com>
2024-11-03 10:57:51 +0700

    feat: add app configuration and update index.vue to display dynamic title

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 const route = useRoute()
+const appConfig = useAppConfig()
+
+console.log(appConfig.theme)
 </script>
+
+
 
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
+    <h1>Nuxt Routing set up successfully! {{ appConfig.title }}</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
